47d467e516137fee7f8a0e446c28a1aa097219ba
Luzemário <luzemario@gmail.com>
2019-02-06 11:20:01 -0200

    Update main.php
    Tradução para português brasileiro

--- templates/main.php
+++ templates/main.php
@@ -2,7 +2,7 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-		<title>APRS Passcode Generator</title>
+		<title>Gerador de senha APRS</title>
 		<style type="text/css" media="screen">
 			body {
 				font-family: Arial, "MS Trebuchet", sans-serif;
@@ -27,18 +27,18 @@
 	<body>
 		<div id="container">
 		
-			<h1>APRS Passcode Generator</h1>
+			<h1>Gerador de senha APRS</h1>
 			
 			<!-- HTML Form to collect callsign. -->
 			<form method="post" action="index.php/passcode">
-				<label for="callsign">Callsign</label>
+				<label for="callsign">Indicativo</label>
 				<input type="text" name="callsign" value="" />
-				<input type="submit" name="Submit" value="Get Passcode" />
+				<input type="submit" name="Submit" value="Pegar Senha" />
 			</form>
 			
 			<!-- Info about the project -->
-			<p>Techical Example of Passcode Generation using PHP</p>
-			<p>Source code available on <a href="https://github.com/magicbug/PHP-APRS-Passcode" title="Github" target="_blank">Github</a></p>
+			<p>Exemplo técnico típico de geração de senha usando PHP</p>
+			<p>Código fonte disponível em <a href="https://github.com/magicbug/PHP-APRS-Passcode" title="Github" target="_blank">Github</a></p>
 		</div>
 	</body>
 
